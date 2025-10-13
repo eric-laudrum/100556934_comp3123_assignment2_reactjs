accf1616743f6a8a54af7a7f525ce5f286bb1298
eric-laudrum <ericlaudrum@gmail.com>
2025-10-12 20:16:34 -0400

    Add employee defining in employee.js

---
+++ employee.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  
+  // Define Employee
+  first_name: { type: String, required: true, trim: true },
+  last_name:  { type: String, required: true, trim: true },
+
+  email:      { type: String, required: true, trim: true, lowercase: true, unique: true },
+  position:   { type: String, required: true, trim: true },
+  salary:     { type: Number, required: true, min: 0 },
+  department: { type: String, required: true, trim: true },
+
+  hired: { type: Date, required: true },
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now }
+}, { collection: 'employees' });
+
+// Save Employee Info
+employeeSchema.pre('save', function(next) {
+  this.updated = new Date();
+  next();
+});
+
+module.exports = mongoose.model('employee', employeeSchema);
