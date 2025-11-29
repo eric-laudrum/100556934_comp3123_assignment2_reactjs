3a15fdad8e779b1c4485f4355f29592086245ece
eric-laudrum <ericlaudrum@gmail.com>
2025-11-29 16:00:20 -0500

    Set up axios to handle http requests

---
+++ frontend/api/axios.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000'
+});
+
+
+// intercept response
+agi.interceptors.request.use(
+    (config) =>{
+        // Get saved token and reformat
+        const token = localStorage.getItem('token');
+
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        // return reformatted config
+        return config;
+    },
+    (error) =>{
+        return Promise.reject(error);
+    }
+);
+
+export default api;
