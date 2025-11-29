af6bc28cc2adb24179d11b9e1148122ebec67978
eric-laudrum <ericlaudrum@gmail.com>
2025-11-29 14:34:39 -0500

    Update user controller to return token in json response

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,27 +6,41 @@ exports.signUp = async (req, res, next)=>{
     const {email, password} = req.body;
 
     try{
+        // Check if user exists
         if(await User.findOne({email})){
             return res.status(400).send({error: 'Email already exists'});
         }
+
+        // Create password & generate jwt token
         const user = await User.create({email, password});
         const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.status(200).json({message: 'User created successfully'});
+        
+        // Return success msg & token
+        res.status(200).json({
+            message: 'User created successfully',
+        token});
     } catch(error){
         next(error);
     }
-}
+};
 
 // Login
 exports.login = async(req, res, next)=>{
     const {email, password} = req.body;
     try{
+        // Verify login info
         const user = await User.findOne({email});
         if(!user || !(await user.matchPassword(password))){
             return res.status(400).send({error: 'Invalid email or password'});
         }
+
+        // Generate JWT token
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.status(200).json({message: 'User logged in successfully'});
+        
+        // Return success msg & token 
+        res.status(200).json({
+            message: 'User logged in successfully',
+            token});
     } catch(e){
         next(e);
     }
